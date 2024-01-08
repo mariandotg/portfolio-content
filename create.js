3ca27084e1f302bfb76f0fa7cb9c4668609c56c4
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-01-08 06:12:59 -0300

    Added base inquirer CLI

--- create.js
+++ create.js
@@ -1,24 +1,54 @@
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');  
+const inquirer = require('inquirer');
 
-if (process.argv.length !== 4) {
-  console.error('Uso: node crear_mdx.js <folder_base> <new_foldername>');
-  process.exit(1);
-}
+const log = {
+  success: (message) => console.log(chalk.green(message)),
+  info: (message) => console.log(chalk.yellow(message)),
+  warn: (message) => console.log(chalk.red(message)),
+  help: (message) => console.log(chalk.violet(message)),
+};
 
-const folderBase = process.argv[2];
-const newFolderName = process.argv[3];
-const folderName = `${folderBase}/${newFolderName}`;
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {name: "title", message:"Write the title of the post", waitUserInput: false},
+    {name: "test", message:"Type of the post", type:"list", choices: ['article', 'project', 'page'], waitUserInput: false}
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(answers)
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+
+const title = "a";
+const type = "y";
+const slug = "l";
+// const title = argv.t || argv.title;
+// const type = argv.T || argv.type;
+// const slug = argv.s || argv.slug;
+
+// const folderBase = process.argv[2];
+// const newFolderName = process.argv[3];
+const folderName = `${type}s/${slug}`;
 const todaysDateRaw = new Date()
 const todaysDate = todaysDateRaw.toISOString()
 const frontmatter = `---
-title: -
+title: ${title}
 image: 
 id: 0
 description: -
 imageAlt: Texto alternativo de la imagen
-slug: ${newFolderName}
-path: ${newFolderName}
+slug: ${slug}
+path: ${slug}
 openGraphType: website
 url: https://marianoguillaume.com
 schemaType: website
@@ -27,7 +57,7 @@ date: ${todaysDate}
 category: Frontmatter
 ---
 
-# Título
+# ${title}
 Este es un archivo MDX de ejemplo en el idioma "es".
 `;
 
